626954f7c0bc8c1c548d5b31db102bea20e8b472
Suyash Mishra <suyashmishra145@gmail.com>
2025-07-20 16:10:25 +0000

    Adding the dumped files directory to gitignore

--- converter/pyvnt_package/test.py
+++ converter/pyvnt_package/test.py
@@ -1,6 +1,6 @@
 from converter import read
 from pyvnt import show_tree
 
-tree_node = read('/opt/openfoam11/tutorials/solidDisplacement/beamEndLoad/system/fvSolution')
+tree_node = read('/workspaces/pyvnt_llm/converter/pyvnt_package/fvSolution.txt')
 
 show_tree(tree_node)
